cde4748002b46a1df67522e889bd7520826f14e1
Jean Flaherty <kobejean@me.com>
2017-12-21 13:07:48 -0700

    Update hello_tf_roll.py

--- hello_tf_roll.py
+++ hello_tf_roll.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 tensor = tf.constant([[1, 2, 3, 4, 5],
                       [6, 7, 8, 9, 10],
                       [11, 12, 13, 14, 15]])
-rolled = tf.roll(tensor, shift=[2,-3], axis=[0,1])
+rolled = tf.manip.roll(tensor, shift=[2,-3], axis=[0,1])
 sess = tf.Session()
 print(rolled)
 print("Before:", sess.run(tensor))
