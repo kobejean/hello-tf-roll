bde3922a992535e09c6f5aff42a84387973ff4ea
Jean Flaherty <kobejean@me.com>
2018-02-06 15:35:45 +0900

    Added docs and uses tf.manip.roll

--- hello_tf_roll.py
+++ hello_tf_roll.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
-manip = tf.load_op_library('user_ops/roll_op.so')
+# use user_ops version
+# manip = tf.load_op_library('user_ops/roll_op.so')
 
 tensor = tf.constant([[1, 2, 3, 4, 5],
                       [6, 7, 8, 9, 10],
                       [11, 12, 13, 14, 15]])
-rolled = manip.custom_roll(tensor, shift=[2,-3], axis=[0,1])
-# rolled = tf.manip.roll(tensor, shift=[2,-3], axis=[0,1])
+# use user_ops version
+# rolled = manip.custom_roll(tensor, shift=[2,-3], axis=[0,1])
+rolled = tf.manip.roll(tensor, shift=[2,-3], axis=[0,1])
 sess = tf.Session()
 print(rolled)
 print("Before:", sess.run(tensor))
